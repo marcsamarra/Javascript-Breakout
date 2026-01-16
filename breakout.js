d9a7dea5e49e0968a507c9917800c75ab52a993b
Marc Samarra <msamarra@gmail.com>
2026-01-16 08:43:59 +0100

    implement paddle and ball movements

--- breakout.js
+++ breakout.js
@@ -11,10 +11,23 @@ let y = canvas.height -30;
 let dx=2;
 let dy=-2;
 
+const paddleHeight =10;
+const paddleWidth =50;
+
+let paddlex = (canvas.width-paddleWidth) /2;
+let paddley = canvas.height - paddleHeight - 10; 
+
+let rightPressed =false;
+let leftPressed =false;
+
+let paddlesensivility = 10;
+
+initEvents()
 draw()
 
 function draw()
 {
+    cleanCanvas()
     drawBall()
     drawPaddle()
     drawBricks()
@@ -26,6 +39,11 @@ function draw()
     window.requestAnimationFrame(draw)
 }
 
+function cleanCanvas()
+{
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+}
+
 function drawBall() {
     ctx.beginPath()
     ctx.arc(x,y,ballRadious,0,Math.PI*2)
@@ -34,7 +52,8 @@ function drawBall() {
 }
 
 function drawPaddle() {
-
+    ctx.fillStyle ='#09F';
+    ctx.fillRect(paddlex,paddley,paddleWidth,paddleHeight);
 }
 
 function drawBricks() {
@@ -42,11 +61,73 @@ function drawBricks() {
 }
 
 function collissionDetection() {
+    if(x>canvas.width || x < 0 ) {
+        dx=dx*-1;
+    }
+ if(y>canvas.height || y < 0) {
+        dy=dy*-1;
+    }
+
+   
 
 }
+
+
+function initEvents()
+{
+    document.addEventListener('keydown',KeyDownHandler)
+    document.addEventListener('keyup',keyUpHandler)
+
+
+
+    function KeyDownHandler (event)
+    {
+        const {key} = event
+        if(key === 'Right' || key === 'ArrowRight')
+        {
+            rightPressed = true;
+        }
+
+        if (key === 'Left' || key === 'ArrowLeft')
+        {
+            leftPressed = true;
+        }
+
+
+
+    }
+
+    function keyUpHandler (event)
+    {
+        const {key} = event
+        if(key === 'Right' || key === 'ArrowRight')
+        {
+            rightPressed = false;
+        }
+
+        if (key === 'Left' || key === 'ArrowLeft')
+        {
+            leftPressed = false;
+        }
+    }
+
+}
+
+
 function ballMovement() {
-
+    x+=dx;
+    y+=dy;
 }
+
+
 function paddleMovement() {
+    if (rightPressed && paddlex < canvas.width-paddleWidth){
+        paddlex += paddlesensivility
+    }
+
+    else if (leftPressed && paddlex > 0){
+        paddlex -= paddlesensivility
+    }
+
 
 }
